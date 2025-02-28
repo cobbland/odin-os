587df8880eae4cc40a6a9f3a22afc76facd12367
cobbland <jacobdensford@proton.me>
2025-02-27 19:05:08 -0500

    Initial setup, layout, and styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
 import "./styles.css";
 
-const pageBody = document.querySelector("body");
-const helloWorld = document.createElement("p");
 
-helloWorld.innerText = "Hello, world!";
-
-pageBody.appendChild(helloWorld);
