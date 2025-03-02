6f5ea3a726200f88da53bf723b5f03df94becfa8
cobbland <jacobdensford@proton.me>
2025-03-02 18:18:27 -0500

    Work on functions

--- src/form.js
+++ src/form.js
@@ -3,45 +3,117 @@ import formHTML from "./form.html";
 const formContainer = document.querySelector('.form');
 formContainer.innerHTML = formHTML;
 
+// get all elements as constants
+const form = document.querySelector('form');
+const email = document.getElementById('email');
+const country = document.getElementById('country');
+const postalCode = document.getElementById('postal-code');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirm-password');
+// const submitButton = document.getElementsByClassName('submit-button')[0];
+const errorMessages = document.getElementsByClassName('error-message')[0];
 
+// validity functions
+function isFormValid() {
+    const emailValid = isEmailValid(email);
+    const countryValid = isCountryValid(country);
+    const postalCodeValid = isPostalCodeValid(country, postalCode);
+    const passwordValid = isPasswordValid(password);
+    const confirmPasswordValid = isConfirmPasswordValid(password, confirmPassword);
+    if (
+        emailValid &&
+        countryValid &&
+        postalCodeValid &&
+        passwordValid &&
+        confirmPasswordValid
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-// Exercises below
-//
-// const form = document.querySelector("form");
-// const email = document.getElementById("mail");
-// const emailError = document.querySelector("#mail + span.error");
-//
-// email.addEventListener("input", () => {
-//     if (email.validity.valid) {
-//         emailError.textContent = ""; // Remove the message content
-//         emailError.className = "error"; // Removes the `active` class
-//     } else {
-//         // If there is still an error, show the correct error
-//         showError();
-//     }
-// });
-//
-// form.addEventListener("submit", (event) => {
-//     // if the email field is invalid
-//     if (!email.validity.valid) {
-//         // display an appropriate error message
-//         showError();
-//         // prevent form submission
-//         event.preventDefault();
-//     }
-// });
-//
-// function showError() {
-//     if (email.validity.valueMissing) {
-//         // If empty
-//         emailError.textContent = "You need to enter an email address.";
-//     } else if (email.validity.typeMismatch) {
-//         // If it's not an email address,
-//         emailError.textContent = "Entered value needs to be an email address.";
-//     } else if (email.validity.tooShort) {
-//         // If the value is too short,
-//         emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
-//     }
-//     // Add the `active` class
-//     emailError.className = "error active";
-// }
+// On the following validity functions, be sure to check data type, right?
+function isEmailValid(email) {
+    console.log(email.value)
+    // Check if email is valid
+    // Return true valid
+    // Otherwise return false
+    if (email.value === '') {
+        addErrorMessage('You must enter an email.')
+        return false;
+    }
+}
+
+function isCountryValid(country) {
+    if (country.value === 'canada' ||
+        country.value === 'mexico' ||
+        country.value === 'united-states'
+    ) {
+        return true;
+    } else if (country.value === 'blank') {
+        addErrorMessage('You must choose a country.')
+        return false;
+    }
+}
+
+function isPostalCodeValid(country, postalCode) {
+    // Check if valid based on country
+    // Return true valid
+    // Otherwise return false
+    if (postalCode.value === '') {
+        addErrorMessage('You must enter a postal code.')
+        return false;
+    }
+}
+
+function isPasswordValid(password) {
+    // Check if valid
+    // Return true valid
+    // Otherwise return false
+    if (password.value === '') {
+        addErrorMessage('You must enter a password.')
+        return false;
+    }
+}
+
+function isConfirmPasswordValid(password, confirmPassword) {
+    // Check if valid and matching password
+    // Return true valid
+    // Otherwise return false
+    if (confirmPassword.value === '') {
+        addErrorMessage('You must confirm your password.')
+        return false;
+    }
+}
+
+// error message functions
+function addErrorMessage(message) {
+    const newMessage = document.createElement('p');
+    newMessage.innerText = `${message}`;
+    errorMessages.appendChild(newMessage);
+}
+
+function clearErrorMessages() {
+    while (errorMessages.firstChild) {
+        errorMessages.removeChild(errorMessages.lastChild);
+    }
+}
+
+// event listeners for each field that checks validity of input (either live or once unfocused)
+// Should call addErrorMessage to give user feedback
+
+// event listener that checks all fields on submit
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formStatus = isFormValid();
+    if (formStatus) {
+        clearErrorMessages();
+        errorMessages.classList.add('thumbs-up');
+        addErrorMessage('Thumbs up!');
+        form.reset();
+    } else {
+        clearErrorMessages();
+        isFormValid()
+    }
+})
