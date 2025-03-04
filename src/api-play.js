a68950fa7b759cd23331c528817872071a4bf3f6
cobbland <jacobdensford@proton.me>
2025-03-04 17:53:18 -0500

    Add weather gif

--- src/api-play.js
+++ src/api-play.js
@@ -26,6 +26,45 @@ class Window {
 
 }
 
-const myAPIWindow = new Window('window-3');
+const myAPIWindow = new Window('window-2');
 myAPIWindow.setWindowTitle('Playing with APIs');
 myAPIWindow.setWindowContent(APIPlayHTML);
+
+const img = document.querySelector('.gif');
+
+let gifSearch = 'waiting';
+
+let weatherLocation = 'ohio';
+
+const newGIF = async function() {
+        fetch(
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${weatherLocation}?key=MBK26J9HC5S79E2LG3RTL4R2W`,
+        {mode: 'cors'}
+    ).then(function(response) {
+        return response.json();
+    }).then(function(response) {
+        gifSearch = response.days[0].conditions
+    }).catch(() => gifSearch = 'try again')
+    .then(function() {
+        fetch(`https://api.giphy.com/v1/gifs/translate?api_key=F6FOppZOtltQDEMqetJa63OW2ptQcrDE&s=${gifSearch}`, {mode: 'cors'})
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            img.src = response.data.images.original.url;
+            weatherText.innerText = gifSearch;
+        });
+    })
+}
+
+newGIF();
+
+const newGIFForm = document.querySelector('#new-gif-form');
+const locationInput = document.querySelector('#location');
+const weatherText = document.querySelector('.weather-text');
+
+newGIFForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    weatherLocation = locationInput.value;
+    newGIF();
+})
